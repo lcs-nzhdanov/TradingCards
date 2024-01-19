10ef723a2cc0fc9d6d2d121103e5a5c78e750bd8
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-19 09:10:25 -0500

    Created trading card structure in different swift file

---
+++ TradingCards/TradingCardProperties.swift
@@ -0,0 +1,22 @@
+//
+//  TradingCardProperties.swift
+//  TradingCards
+//
+//  Created by Nikita Zhdanov on 2024-01-19.
+//
+
+import Foundation
+
+struct TrdingCard {
+    let driverName: String
+    let driverSurname: String
+    let driverTeam: String
+    let teamImageName: String
+    
+    let careerPoints: Int
+    let careerWins: Int
+    let careerPodiums: Int
+    let careerPoles: Int
+    
+    let famousRace: String
+}
