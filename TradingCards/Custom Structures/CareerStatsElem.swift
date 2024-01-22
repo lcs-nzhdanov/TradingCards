d2ea8155ed7916ba767b8d07ca7d5c2b25b8b33e
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-21 19:56:08 -0500

    Created a custom structure to list stats

---
+++ TradingCards/Custom Structures/CareerStatsElem.swift
@@ -0,0 +1,18 @@
+//
+//  CareerStatsView.swift
+//  TradingCards
+//
+//  Created by Nikita Zhdanov on 2024-01-21.
+//
+
+import SwiftUI
+
+struct CareerStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CareerStatsView()
+}
